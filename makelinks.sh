89cf7ef16efc506548bb10b6f02d79ba8a903d2c
Quinn Miller <millerquinn66@gmail.com>
2020-02-16 12:51:26 -0800

    Add link to hl2 theme

--- makelinks.sh
+++ makelinks.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/hl1.zsh-theme ~/.oh-my-zsh/themes/hl1.zsh-theme
+ln -s ~/.dotfiles/hl2.zsh-theme ~/.oh-my-zsh/themes/hl2.zsh-theme
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 
 #Orinal font name is Droid Sans Mono for Powerline
