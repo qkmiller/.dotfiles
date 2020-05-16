c3fe2b75b7039e18c360344a870d4eee077b190e
Quinn Miller <millerquinn66@gmail.com>
2020-05-16 00:53:44 -0700

    Fix git-prompt

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,7 +1,7 @@
   PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
-	PS1="$PS1"'\[\033[95m\]'       # change to cyan
+	PS1="$PS1"'\[\033[95m\]'       # change to magenta
 	PS1="$PS1"'\w'                 # current working directory
 	if test -z "$WINELOADERNOEXEC"
 	then
@@ -13,10 +13,10 @@
 		then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-			PS1="$PS1"'`__git_ps1`'   # bash function
+			PS1="$PS1"'\[\033[36m\]'   # change color to cyan
+			PS1="$PS1"'`__git_ps1`'    # bash function
 		fi
 	fi
 	PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'\[\033[39m\]'       # change to cyan
+	PS1="$PS1"'\[\033[0m\]'        # change to white
 	PS1="$PS1"'$ '                 # prompt
