9f68638e0bce04fbf2cf0e0856921d87f4730731
Quinn Miller <millerquinn66@gmail.com>
2020-06-20 13:04:59 -0700

    Create directories for related files

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ mkdir -p ~/.vim/tmp/undo
 # Make directories before linking
 mkdir -p ~/.oh-my-zsh/themes
 
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/hl2.zsh-theme ~/.oh-my-zsh/themes/hl2.zsh-theme
-ln -s ~/dotfiles/vimrc ~/.vim/vimrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gitignore ~/.gitignore_global
+# Create symlinks
+ln -s ~/dotfiles/zsh/hl2.zsh-theme ~/.oh-my-zsh/themes/hl2.zsh-theme
+ln -s ~/dotfiles/vim/vimrc ~/.vim/vimrc
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore ~/.gitignore_global
 
 # Install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -22,4 +22,5 @@ vim +PlugInstall +qall
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
+# Overwrite the default zshrc with mine
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
