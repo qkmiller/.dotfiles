290fe88fd918c823fa095dc9c15442e5e2e7e691
Quinn Miller <millerquinn66@gmail.com>
2020-11-02 20:21:03 -0800

    Add desktop notes and fix bold prompt

--- setup-all.sh
+++ setup-all.sh
@@ -9,3 +9,7 @@ echo "Setting up Tmux config..."
 source ./setup-tmux.sh
 echo "Setting up Zsh config..."
 source ./setup-zsh.sh
+# Create notes directory and file on desktop
+echo "Creating /home/$USER/Desktop/notes/notes.md"
+mkdir -p ~/Desktop/notes
+touch ~/Desktop/notes/notes.txt
