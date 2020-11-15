cc7cf2a8a13c9c1ffd615a642d7077be9fb71d84
Quinn Miller <millerquinn66@gmail.com>
2020-11-14 17:21:19 -0800

    Lower bit-depth and add detailed usage info

--- scripts/streamaudio.sh
+++ scripts/streamaudio.sh
@@ -7,7 +7,7 @@ then
   then
     echo "Audio streaming already enabled"
   else
-    pacmd "load-module module-tunnel-sink-new sink_name=livingroom format=float32le rate=44100 server=$PASERVER"
+    pacmd "load-module module-tunnel-sink-new sink_name=livingroom format=s16 rate=44100 server=$PASERVER"
     pacmd 'set-default-sink livingroom'
   fi
 elif [ "$1" == off ]
@@ -19,10 +19,14 @@ then
     pacmd 'unload-module module-tunnel-sink-new'
   fi
 else
-  echo "Stream all sound to a PulseAudio server on your network."
-  echo "Example:"
-  echo "streamaudio [on/off]"
-  echo "To change the server address, export \$PASERVER in your bash/zshrc."
-  echo "Example:"
-  echo "export PASERVER=193.168.0.57"
+  echo "Stream your system's audio to a remote PulseAudio server."
+  echo "Usage:  streamaudio [on | off]"
+  echo "    e.g., you@yourhost\$ streamaudio on"
+  echo "          you@yourhost\$ streamaudio off"
+  echo "To change the server address, add 'export \$PASERVER={address}' to your .bashrc/.zshrc."
+  echo "    e.g., you@yourhost\$ echo 'export PASERVER=193.168.0.57' >> ~/.bashrc"
+  echo "On the remote server, use pacmd to load the tcp module and authorize a single"
+  echo "IP address, a range of IP addresses, or all IP adresses."
+  echo "    e.g., you@yourhost\$ pacmd load-module module-native-protocol-tcp auth-ip-acl=172.225.0.95"
+  echo "    e.g., you@yourhost\$ pacmd load-module module-native-protocol-tcp auth-anonymous=true"
 fi
